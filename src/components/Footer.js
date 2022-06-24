1dd6ea21547c609cf161d426c67a31b2bab8b0e4
John Lanni <Johnlanni619@gmail.com>
2022-06-24 17:34:04 -0400

    updated footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
-import { ExternalLink } from 'react-external-link';
-import { GoMarkGithub } from 'react-icons/go';
-import { FaLinkedin } from 'react-icons/fa';
+import { ExternalLink } from "react-external-link";
+import { GoMarkGithub } from "react-icons/go";
+import { FaLinkedin } from "react-icons/fa";
 
-export default function Footer () {
-    return (
-        <footer>
-            <div id="footer-icon-container">
-                <ExternalLink href="https://github.com/JohnLanni619">
-                    <span><GoMarkGithub/></span>
-                </ExternalLink>
-                <ExternalLink href="https://www.linkedin.com/in/john-lanni-45765711a/">
-                    <span><FaLinkedin/></span>
-                </ExternalLink>
-            </div>
-        </footer>
-    )
-};
+export default function Footer() {
+  return (
+    <footer>
+      <div id="footer-icon-container">
+        <ExternalLink href="https://github.com/JohnLanni619">
+          <GoMarkGithub className="icon" />
+        </ExternalLink>
+        <ExternalLink href="https://www.linkedin.com/in/john-lanni-45765711a/">
+          <FaLinkedin className="icon" />
+        </ExternalLink>
+      </div>
+    </footer>
+  );
+}
